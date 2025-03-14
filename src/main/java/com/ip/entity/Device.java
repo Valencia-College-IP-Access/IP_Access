23ec53b7362f427a68d4547944b4f8fc20ef4e43
Justin Winkler <152222211+ThatGuyWink@users.noreply.github.com>
2025-03-14 15:35:21 -0400

    Add files via upload

---
+++ src/main/java/com/ip/entity/Device.java
@@ -0,0 +1,88 @@
+package com.ip.entity;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "Devices", schema = "IP_Access_Project_DB")
+public class Device {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "deviceID")
+    private Integer id;
+
+    @Column(name = "deviceName", length = 50)
+    private String deviceName;
+
+    @ColumnDefault("1")
+    @Column(name = "availability", nullable = false)
+    private Boolean availability = false;
+
+    @OnDelete(action = OnDeleteAction.SET_NULL)
+    @Column(name = "renterID")
+    private Integer renterID;
+
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "created_at")
+    private Instant createdAt;
+
+    public Device() {
+
+    }
+
+    public Device(String deviceName, boolean availability) {
+        setDeviceName(deviceName);
+        setAvailability(availability);
+    }
+
+    public Device(String deviceName, boolean availability, int renterID) {
+        setDeviceName(deviceName);
+        setAvailability(availability);
+        setRenterID(renterID);
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDeviceName() {
+        return deviceName;
+    }
+
+    public void setDeviceName(String deviceName) {
+        this.deviceName = deviceName;
+    }
+
+    public Boolean getAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(Boolean availability) {
+        this.availability = availability;
+    }
+
+    public Integer getRenterID() {
+        return renterID;
+    }
+
+    public void setRenterID(Integer renterID) {
+        this.renterID = renterID;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+}
---
+++ src/main/java/com/ip/entity/User.java
@@ -0,0 +1,115 @@
+package com.ip.entity;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "Users", schema = "IP_Access_Project_DB")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "userID", nullable = false)
+    private Integer id;
+
+    @Column(name = "firstName", nullable = false, length = 50)
+    private String firstName;
+
+    @Column(name = "lastName", nullable = false, length = 50)
+    private String lastName;
+
+    @Column(name = "zipCode", length = 10)
+    private String zipCode;
+
+    @Column(name = "email", nullable = false, length = 100)
+    private String email;
+
+    @Column(name = "phoneNumber", length = 20)
+    private String phoneNumber;
+
+    @Column(name = "created_at", updatable = false, insertable = false)
+    private Instant createdAt;
+
+    public User() {
+
+    }
+    public User(String firstName, String lastName, String email, String phoneNumber) {
+        setFirstName(firstName);
+        setLastName(lastName);
+        setEmail(email);
+        setPhoneNumber(phoneNumber);
+    }
+    public User(String firstName, String lastName, String email) {
+        setFirstName(firstName);
+        setLastName(lastName);
+        setEmail(email);
+    }
+    public User(String firstName, String lastName, String zipCode, String email, String phoneNumber) {
+        setFirstName(firstName);
+        setLastName(lastName);
+        setZipCode(zipCode);
+        setEmail(email);
+        setPhoneNumber(phoneNumber);
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", zipCode='" + zipCode + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", createdAt=" + createdAt +
+                '}';
+    }
+}
